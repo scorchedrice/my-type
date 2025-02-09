f2e2c91e4a43d15054422fe76631f5f60919482e
scorchedrice <woongveloper98@gmail.com>
2025-02-09 23:46:36 +0900

    :memo: post : 한지웅-2.5,2.6.md

--- 1주차/한지웅/codes/sampleCode.ts
+++ 1주차/한지웅/codes/sampleCode.ts
@@ -29,3 +29,43 @@ const result2 : string = connectWord('Hello', 'TypeScript')
 
 const obj = { name : 'han'}; // const obj : {name:string}
 const arr = [1,3,'five']; // const arr : ( string| number )[];
+
+// 차이
+const name_string : string = 'ji-woong';
+const name_String1 : String = new String('ji-woong');
+const name_String2 : String = new String('Han');
+// 사용의 제한
+// console.log(name_String1 + name_String2)
+
+function test(a: number, b: number) {
+  return a+b;
+}
+
+const typeTest : typeof test = (x : number, y : number) => x+y;
+
+
+class Wizard {
+  power : number;
+  constructor(
+    hp : number,
+    mp : number,
+    int : number,
+    nickname : string
+  ) {
+    this.power = hp + mp + int;
+  }
+}
+
+const myCharacter : Wizard = new Wizard(100, 350, 200, '초보자');
+console.log(myCharacter); // Wizard { power: 650 }
+
+
+function toNumber(input : string | number) {
+  if (typeof input === 'string') {
+    // if문에 걸렸으니 자연스럽게 string으로 추론
+    return parseInt(input)
+  }
+
+  // if를 확인한 결과 아님이 판단되었으므로 number로 추론
+  return input;
+}
