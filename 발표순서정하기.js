b92685139d9f3b375f304dbef8d2c82e2fd387d6
scorchedrice <wldnd2977@gmail.com>
2025-02-12 02:10:42 +0900

    💡update: 한지웅-2.8-2.9.md, 발표순서정하기.js

---
+++ 발표순서정하기.js
@@ -0,0 +1,17 @@
+const studyMembers = ['김종현', '김희영', '신혜민', '조민우', '최인애', '한지웅'];
+const topics = ['a', 'b', 'c', 'd', 'e', 'f'];
+
+function shuffle(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
+}
+
+const shuffledMembers = shuffle(studyMembers);
+console.log('-----------')
+for (let i = 0; i < 6; i++) {
+    console.log(`${topics[i]}: ${shuffledMembers[i]}`);
+}
+console.log('-----------')
